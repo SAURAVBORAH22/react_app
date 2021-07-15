d9f50670c8be13673301d43e648ee3f620704eaa
SAURAVBORAH22 <sauravborah1999@gmail.com>
2021-07-15 17:32:47 +0530

    header and footer

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,7 +4,6 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "re
 function RenderMenuItem({ dish, onClick }) {
     return (
         <Card onClick={() => onClick(dish.id)} >
-
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle><strong> {dish.name} </strong></CardTitle>
